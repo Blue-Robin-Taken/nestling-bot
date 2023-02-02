44f9622de579846bff01ecf2233b4950afb140e6
Blue Robin <julianjohnt@gmail.com>
2023-02-02 00:29:27 -0600

    Fixed bug in randomvideofromchannel

--- Fun/requestsfun.py
+++ Fun/requestsfun.py
@@ -12,39 +12,51 @@ class testYoutube(commands.Cog):
         self.bot = bot
         self.key = "AIzaSyC0QBEb_cSWCqOO8rbPG6t7edN-sFugslQ"
 
+    async def randomvideoerr(self, ctx):
+        await ctx.respond(
+            f"I couldn't complete that request. Check perms, or change link.",
+            ephemeral=True)
     @commands.slash_command(name="randomvideofromchannel")
     async def randomvideofromchannel(self, ctx, url: str):
         try:
-            tabs = ['featured', 'community', 'playlists', 'videos', 'channels', 'about']
-            try:
-                channel_id = url.split('/')[4]
-                if channel_id in tabs:
-                    channel_name = url.split('/')[3].replace('@', '')
-                    search_url = f"https://www.googleapis.com/youtube/v3/search?part=id%2Csnippet&q={channel_name}&type=channel&key={self.key}"
-                    response = requests.get(search_url)
-                    channel_id = response.json()['items'][0]['id']['channelId']
-            except IndexError:
+            if not url.split('/')[2] == 'www.youtube.com':
+                await self.randomvideoerr(ctx)
+                return
+            if len(url.split('/')) == 0:
+                await self.randomvideoerr(ctx)
+                return
+        except IndexError:
+            await self.randomvideoerr(ctx)
+            return
+
+        tabs = ['featured', 'community', 'playlists', 'videos', 'channels', 'about', 'items']
+        try:
+            channel_id = url.split('/')[4]
+            if channel_id in tabs:
                 channel_name = url.split('/')[3].replace('@', '')
                 search_url = f"https://www.googleapis.com/youtube/v3/search?part=id%2Csnippet&q={channel_name}&type=channel&key={self.key}"
                 response = requests.get(search_url)
-                channel_id = response.json()['items'][0]['id']['channelId']
+                try:
+                    channel_id = response.json()['items'][0]['id']['channelId']
+                except KeyError:
+                    print(response.json())
+        except IndexError:
+            channel_name = url.split('/')[3].replace('@', '')
+            search_url = f"https://www.googleapis.com/youtube/v3/search?part=id%2Csnippet&q={channel_name}&type=channel&key={self.key}"
+            response = requests.get(search_url)
+            channel_id = response.json()['items'][0]['id']['channelId']
 
-            url = f"https://www.googleapis.com/youtube/v3/search?key={self.key}&channelId={channel_id}&part=snippet,id&order=date&maxResults=10000&type=video"
-            response = requests.get(url)
-            data = response.json()
-            data_again = random.choice(data['items'])['id']
-            send_url = "ERROR"
-            try:
-                send_url = "https://www.youtube.com/watch?v=" + data_again['videoId']
-            except KeyError:
-                print(data_again)
+        url = f"https://www.googleapis.com/youtube/v3/search?key={self.key}&channelId={channel_id}&part=snippet,id&order=date&maxResults=10000&type=video"
+        response = requests.get(url)
+        data = response.json()
+        data_again = random.choice(data['items'])['id']
+        send_url = "ERROR"
+        try:
+            send_url = "https://www.youtube.com/watch?v=" + data_again['videoId']
+        except KeyError:
+            print(data_again)
 
-            await ctx.respond(send_url)
-        except (IndexError, KeyError):
-            await ctx.respond(
-                f"I couldn't complete that request. Check perms, or change link.",
-                ephemeral=True)
-            return
+        await ctx.respond(send_url)
 
     @randomvideofromchannel.error
     async def on_application_command_error(ctx: discord.ApplicationContext, error: discord.DiscordException):
